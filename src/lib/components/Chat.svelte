075ab5428fd6724ab91b555807b757c03c59751a
Herbe Malveillante <herbemalveillante@gmail.com>
2021-11-12 16:23:31 +0100

    starting to implement top bar

--- src/lib/components/Chat.svelte
+++ src/lib/components/Chat.svelte
@@ -1,3 +1,19 @@
-<div class="flex flex-1 h-screen bg-dark3 text-white items-center justify-center"> 
-    Chat
-</div>
+<main class="flex flex-1 h-screen bg-dark3 text-white">
+    <div class="flex h-14 w-full shadow border-b border-black items-center justify-between">
+        <div class="flex border-red border-2 text-lg font-medium ml-6 items-center">
+            <svg class="w-6 h-6 mr-1.5 text-darkgray text-xl font-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14" /></svg>
+            <span>Medias</span>
+        </div>
+        <div class="mr-6">
+            <div>
+                <!-- threads icon -->
+                <!-- notifications icons -->
+                <!-- pinned icon -->
+                <!-- member list icon -->
+                <!-- search bar -->
+                <!-- inbox icon -->
+                <!-- help icon -->
+            </div>
+        </div>
+    </div>
+</main>
